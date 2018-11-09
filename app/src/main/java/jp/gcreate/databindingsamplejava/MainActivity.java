761f49d61aac03cb6265c75c89e130a90ae51606
Gen <archiherewego@gmail.com>
2018-11-10 00:00:52 +0900

    Set button enabled only name is not empty

--- app/src/main/java/jp/gcreate/databindingsamplejava/MainActivity.java
+++ app/src/main/java/jp/gcreate/databindingsamplejava/MainActivity.java
@@ -1,13 +1,35 @@
 package jp.gcreate.databindingsamplejava;
 
+import android.arch.lifecycle.ViewModelProviders;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import jp.gcreate.databindingsamplejava.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MainViewModel mainViewModel;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        mainViewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+
+        binding.setVm(mainViewModel);
+
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,
+                               "Hello " + mainViewModel.getName(),
+                               Toast.LENGTH_SHORT)
+                     .show();
+            }
+        });
     }
 }
---
+++ app/src/main/java/jp/gcreate/databindingsamplejava/MainViewModel.java
@@ -0,0 +1,40 @@
+package jp.gcreate.databindingsamplejava;
+
+import android.arch.lifecycle.ViewModel;
+import android.databinding.Bindable;
+import android.databinding.Observable;
+import android.databinding.PropertyChangeRegistry;
+
+/**
+ * Copyright 2018 G-CREATE
+ */
+public class MainViewModel extends ViewModel implements Observable {
+
+    private PropertyChangeRegistry callbacks = new PropertyChangeRegistry();
+    @Bindable
+    private String name = "";
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        callbacks.notifyChange(this, BR.buttonEnabled);
+        this.name = name;
+    }
+
+    @Bindable
+    public boolean isButtonEnabled() {
+        return !name.isEmpty();
+    }
+
+    @Override
+    public void addOnPropertyChangedCallback(OnPropertyChangedCallback callback) {
+        callbacks.add(callback);
+    }
+
+    @Override
+    public void removeOnPropertyChangedCallback(OnPropertyChangedCallback callback) {
+        callbacks.remove(callback);
+    }
+}
